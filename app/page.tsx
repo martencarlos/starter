b8ddf2e49d664133ebe0e5e330a7357060b503fc
Carlos Marten <martencarlos@gmail.com>
2025-05-07 21:06:22 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+/**
+ * The main page component that renders the HomePage component.
+ */
+const Page = () => {
+    return (
+        <div className='flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4'>
+            <h1 className='text-4xl font-bold text-gray-800'>Welcome to the Home Page</h1>
+            <p className='mt-4 text-lg text-gray-600'>This is a simple example of a Next.js page.</p>
+        </div>
+    );
+};
+
+export default Page;
---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+import initializeBundleAnalyzer from '@next/bundle-analyzer';
+
+// https://www.npmjs.com/package/@next/bundle-analyzer
+const withBundleAnalyzer = initializeBundleAnalyzer({
+    enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
+});
+
+// https://nextjs.org/docs/pages/api-reference/next-config-js
+const nextConfig: NextConfig = {
+    output: 'standalone',
+    turbopack: {
+        resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json']
+    }
+};
+
+export default withBundleAnalyzer(nextConfig);
